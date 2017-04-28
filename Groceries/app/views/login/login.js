dafa8bca383d04e7d59a1e6b8ab848e3b006bed3
Cicero Artifon <c1285717@bb.com.br>
2017-04-28 18:06:42 -0300

    Creating login logic

---
+++ Groceries/app/views/login/login.js
@@ -0,0 +1,20 @@
+var UserViewModel = require("../../shared/view-models/user-view-model");
+var frameModule = require("ui/frame");
+var page;
+var email;
+
+var user = new UserViewModel();
+
+exports.loaded = function(args) {
+    page = args.object;
+    page.bindingContext = user;
+};
+
+exports.signIn = function() {
+    user.login();
+};
+
+exports.register = function() {
+    var topmost = frameModule.topmost();
+    topmost.navigate("views/register/register");
+};
