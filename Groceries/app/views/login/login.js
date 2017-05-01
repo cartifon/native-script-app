bf0d7bd2a4fefbb959192f222bba37524566e3f6
Cicero Artifon <c1285717@bb.com.br>
2017-05-01 13:05:36 -0300

    4.2: Building dialog module

--- Groceries/app/views/login/login.js
+++ Groceries/app/views/login/login.js
@@ -1,4 +1,7 @@
 var UserViewModel = require("../../shared/view-models/user-view-model");
+
+var dialogsModule = require("ui/dialogs");
+
 var frameModule = require("ui/frame");
 var page;
 var email;
@@ -11,7 +14,18 @@ exports.loaded = function(args) {
 };
 
 exports.signIn = function() {
-    user.login();
+    user.login()
+        .catch(function(error) {
+            console.log(error);
+            dialogsModule.alert({
+                message: "Unfortunately we could not find your account.",
+                okButtonText: "OK"
+            });
+            return Promise.reject();
+        })
+        .then(function() {
+            frameModule.topmost().navigate("views/list/list");
+        });
 };
 
 exports.register = function() {
