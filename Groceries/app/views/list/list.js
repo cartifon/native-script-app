5481d12e4dcf1b5580df788479aa494d67e7cbd5
cartifon <ciceroartifon@gmail.com>
2017-05-04 20:51:50 -0300

    4.7: Animations
    Creating an animation to show the itens

--- Groceries/app/views/list/list.js
+++ Groceries/app/views/list/list.js
@@ -13,6 +13,7 @@ var pageData = new observableModule.fromObject({
 
 exports.loaded = function(args) {
     page = args.object;
+    var listView = page.getViewById("groceryList");
     page.bindingContext = pageData;
 
     groceryList.empty();
@@ -20,6 +21,10 @@ exports.loaded = function(args) {
     pageData.set("isLoading", true);
     groceryList.load().then(function() {
         pageData.set("isLoading", false);
+        listView.animate({
+            opacity: 1,
+            duration: 1000
+        });
     });
 };
 
